56ce66a5025bac86971568865c25d32cc574a614
herom <h.romirer@gmail.com>
2014-10-13 06:12:23 +0200

    [FIX] Handle results with no matches in SoapClient utility.

--- util/soap-client.js
+++ util/soap-client.js
@@ -34,9 +34,9 @@ var SoapClient = {
     }
     var client = this.client;
 
-    return new Promise(function promisedDictionaryLookup (resolve, reject) {
+    return new Promise(function promisedDictionaryLookup(resolve, reject) {
       client.DictionaryList({}, function (err, list) {
-        if(err) {
+        if (err) {
           reject(err);
         } else {
           resolve(list.DictionaryListResult.Dictionary);
@@ -46,55 +46,56 @@ var SoapClient = {
   },
 
   lookupDefinition: function (word) {
-    if(!this.isClientSet) {
+    if (!this.isClientSet) {
       return Promise.reject('No SoapClient available!');
     }
 
     var self = this;
 
-    return new Promise( function promisedWordLookup (resolve, reject) {
+    return new Promise(function promisedWordLookup(resolve, reject) {
       self.client.Define({word: word.trim()}, function (err, defined) {
-        if(err) {
+        if (err) {
           reject(err);
         } else {
           var definitions = defined.DefineResult.Definitions.Definition;
           var preparedDefinitions = [];
 
-          definitions.forEach(function (definition) {
-            var dictionary = definition.Dictionary.Name;
-            var textSplits = definition.WordDefinition.split('[syn:');
-            var text = definition.WordDefinition;
-            var word = definition.Word;
-            var def = '';
-            var synonyms;
-            var prepared = {};
+          if (definitions) {
+            definitions.forEach(function (definition) {
+              var dictionary = definition.Dictionary.Name;
+              var textSplits = definition.WordDefinition.split('[syn:');
+              var text = definition.WordDefinition;
+              var word = definition.Word;
+              var def = '';
+              var synonyms;
+              var prepared = {};
 
-            if(Array.isArray(textSplits) && textSplits.length > 1) {
-              text = textSplits[0];
-              synonyms = self._extractSynonyms(textSplits[1].replace('\n', '').replace(/\s{2,}/g, ' '));
-            }
+              if (Array.isArray(textSplits) && textSplits.length > 1) {
+                text = textSplits[0];
+                synonyms = self._extractSynonyms(textSplits[1].replace('\n', '').replace(/\s{2,}/g, ' '));
+              }
 
-            text = text.replace('\n', '').replace(/\s{2,}/g, ' ').trim();
+              text = text.replace('\n', '').replace(/\s{2,}/g, ' ').trim();
 
-            if(/^:/.exec(text)) {
-              def = text.substr(1, text.length);
-            } else {
-              def = text;
-            }
+              if (/^:/.exec(text)) {
+                def = text.substr(1, text.length);
+              } else {
+                def = text;
+              }
 
-            prepared = {
-              word: word,
-              def: def,
-              dict: dictionary
-            };
+              prepared = {
+                word: word,
+                def: def,
+                dict: dictionary
+              };
 
-            if(Array.isArray(synonyms) && synonyms.length > 0) {
-              prepared.syn = synonyms;
-            }
+              if (Array.isArray(synonyms) && synonyms.length > 0) {
+                prepared.syn = synonyms;
+              }
 
-            preparedDefinitions.push(prepared);
-          });
-
+              preparedDefinitions.push(prepared);
+            });
+          }
           resolve(preparedDefinitions);
         }
       });
@@ -106,7 +107,7 @@ var SoapClient = {
     var regExp = /{([^}]+)}/g;
     var text;
 
-    while(text = regExp.exec(str)) {
+    while (text = regExp.exec(str)) {
       synonyms.push(text[1]);
     }
 
